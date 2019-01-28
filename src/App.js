3b021e06fbf332da29f29e83927fe32a4258cf58
rahul7822 <rs.dangwal78223@gamil.com>
2019-01-28 23:03:33 +0530

    state , props & React development tool extension
    1.pass states from App.js
    2.assign this as a property of Todos component
    3.acess it form the Todos.js file

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+    state={
+        todos:[
+            {
+                id:1,
+                title: "Take out the trash",
+                completed: false
+            },
+            {
+                id:2,
+                title: "Dinner with wife",
+                completed: false
+            },
+            {
+                id:3,
+                title: "Meeting with boss",
+                completed: false
+            }
+        ]
+    }
+
     render() {
+       // console.log(this.state.todos);
         return ( 
         <div className = "App" >
-             <Todos></Todos>
+             <Todos todos={this.state.todos}/>
         </div>
         );
     }
