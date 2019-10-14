064dc794646c5266cad52000e8b964b9e5203c08
Rahul Singh <rs.dangwal78223@gmail.com>
2019-10-14 21:39:14 +0530

    Update App.js
    axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
            axios.delete('https://jsonplaceholder.typicode.com/todos/{id}')

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
     //delete TodoItem
     delTodo=(id)=>{
-        axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
+        axios.delete('https://jsonplaceholder.typicode.com/todos/{id}')
         .then(res => this.setState({todos:[...this.state.todos.filter(todo=>
             todo.id !== id)]}));
     }
